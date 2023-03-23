16453dedcef329ba9c9c38be22f1af2168eef0b4
nazmieievilya <ilyanazmeev111@gmail.com>
2023-03-23 14:19:52 +0300

    Knight moves

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cells = document.querySelectorAll(".cell");
 const board = document.querySelector(".board");
-const defaultArr = [
+let defaultArr = [
   [
     [0, "♜"],
     [0, "♞"],
@@ -23,17 +23,52 @@ const defaultArr = [
   ],
   [" ", " ", " ", " ", " ", " ", " ", " "],
   [" ", " ", " ", " ", " ", " ", " ", " "],
-  [" ", " ", " ", [1, "♟"], " ", " ", " ", " "],
-  [" ", " ", [1, "♔"], " ", " ", " ", " ", " "],
   [" ", " ", " ", " ", " ", " ", " ", " "],
   [" ", " ", " ", " ", " ", " ", " ", " "],
+  [
+    [1, "♟"],
+    [1, "♟"],
+    [1, "♟"],
+    [1, "♟"],
+    [1, "♟"],
+    [1, "♟"],
+    [1, "♟"],
+    [1, "♟"],
+  ],
+  [
+    [1, "♜"],
+    [1, "♞"],
+    [1, "♝"],
+    [1, "♛"],
+    [1, "♔"],
+    [1, "♝"],
+    [1, "♞"],
+    [1, "♜"],
+  ],
 ];
 
-function render(arr) {
-  const twoDemArr = arr.map((_, i) => {
+function calcKnightMoves(knight, horse) {
+  return [
+    [knight[0] + 1, knight[1] + 2],
+    [knight[0] + 1, knight[1] - 2],
+    [knight[0] + 2, knight[1] + 1],
+    [knight[0] + 2, knight[1] - 1],
+    [knight[0] - 1, knight[1] - 2],
+    [knight[0] - 1, knight[1] + 2],
+    [knight[0] - 2, knight[1] - 1],
+    [knight[0] - 2, knight[1] + 1],
+  ].filter((coord) => {
+    if (coord[0] < 8 && coord[0] >= 0 && coord[1] < 8 && coord[1] >= 0)
+      return true;
+  });
+}
+const twoDamnArr = (arr) =>
+  arr.map((_, i) => {
     if (i > 7) return;
     return [...cells].slice(i * 8, i * 8 + 8);
   });
+function render(arr) {
+  const twoDemArr = twoDamnArr(arr);
   twoDemArr.forEach((row, x) => {
     row.forEach((cell, y) => {
       if (arr[x][y] == " ") return;
@@ -41,7 +76,7 @@ function render(arr) {
       const figure = arr[x][y][1];
       cell.insertAdjacentHTML(
         "afterbegin",
-        `<p data-coords='${x},${x}' class='${
+        `<p data-coords='${x},${y}' class='${
           color > 0 ? "balck" : "white"
         } figure'>${figure}</p>`
       );
@@ -49,12 +84,52 @@ function render(arr) {
   });
 }
 
+function drawMoves(moves, arr) {
+  const twoDemArr = twoDamnArr(arr);
+  moves.forEach((move) => {
+    twoDemArr[move[0]][move[1]].classList.add("hightlight");
+    twoDemArr[move[0]][move[1]].dataset.coords = [move[0], move[1]].join(",");
+  });
+}
+function clear() {
+  cells.forEach((cell) => {
+    cell.innerHTML = "";
+  });
+}
+
+function moveFigure(figure, arr, coordsToMove) {
+  console.log(coordsToMove);
+  console.log(figure);
+  const newArr = JSON.parse(JSON.stringify(arr));
+  newArr[+coordsToMove[0]][+coordsToMove[1]] = newArr[figure[0]][figure[1]];
+  newArr[figure[0]][figure[1]] = " ";
+  defaultArr = newArr;
+  clear();
+  render(newArr);
+  cells.forEach((cell) => cell.classList.remove("hightlight"));
+}
+let selectedFig = "";
 function run(e) {
+  if (e.target.classList.contains("hightlight"))
+    return moveFigure(
+      selectedFig,
+      defaultArr,
+      e.target.dataset.coords.split(",")
+    );
+  cells.forEach((cell) => cell.classList.remove("hightlight"));
   const element = e.target.closest(".cell").children[0];
   if (!element) return;
   const coords = element.dataset.coords.split(",").map((el) => +el);
   const figure = [element.classList[0] == "white" ? 0 : 1, element.textContent];
-  console.log(figure, coords);
+  selectedFig = coords;
+  if (figure[1] === "♞") {
+    const moves = calcKnightMoves(coords);
+    const availableMoves = moves.filter((move) => {
+      if (defaultArr[move[0]][move[1]] == " ") return true;
+    });
+    // rendering
+    drawMoves(availableMoves, defaultArr);
+  }
 }
 
 render(defaultArr);
